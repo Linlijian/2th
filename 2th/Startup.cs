3d727fad82398933b5951a0eb178a5fd82109583
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-03-14 02:31:04 +0700

    Add project files.

---
+++ 2th/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2th.Startup))]
+namespace _2th
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
